6aaae74da9d2204799722619d8240805c85b7fec
Dion Häfner <dion.haefner@nbi.ku.dk>
2020-05-25 12:43:47 +0200

    tweak postprocessing

--- fowd/postprocessing.py
+++ fowd/postprocessing.py
@@ -7,26 +7,36 @@ import tqdm
 from .constants import QC_EXTREME_WAVE_LOG_THRESHOLD
 
 
-def plot_qc(qcfile, outdir, plot_flags=tuple('abdg'), plot_extreme=True):
+def plot_qc(qcfile, outdir, exclude_flags=tuple('cefg'), plot_extreme=True):
     import matplotlib as mpl
     mpl.use('Agg')
     import matplotlib.pyplot as plt
 
-    if not set(plot_flags) <= set('abcdefg'):
-        raise ValueError('plot_flags can only contain {a, b, c, d, e, f, g}')
+    if not set(exclude_flags) <= set('abcdefg'):
+        raise ValueError('exclude_flags can only contain {a, b, c, d, e, f, g}')
 
     with open(qcfile, 'r') as f:
         qc_records = [json.loads(line) for line in f]
 
     os.makedirs(outdir, exist_ok=True)
 
+    basename = os.path.basename(qcfile).split('.')[0]
+
     i = 1
 
     for record in tqdm.tqdm(qc_records):
-        process_record = any(flag in plot_flags for flag in record['flags_fired'])
+        qc_passed = len(record['flags_fired']) == 0
+
+        process_record = (
+            not qc_passed
+            and all(flag not in exclude_flags for flag in record['flags_fired'])
+        )
 
         if plot_extreme:
-            process_record |= record['relative_wave_height'] > QC_EXTREME_WAVE_LOG_THRESHOLD
+            process_record |= (
+                qc_passed
+                and record['relative_wave_height'] > QC_EXTREME_WAVE_LOG_THRESHOLD
+            )
 
         if not process_record:
             continue
@@ -39,11 +49,13 @@ def plot_qc(qcfile, outdir, plot_flags=tuple('abdg'), plot_extreme=True):
 
         elev_range = np.nanmax(np.abs(record['elevation']))
 
-        info = [
-            f'QC flags fired: {record["flags_fired"]}' if record['flags_fired'] else 'QC passed',
+        info_left = [
             f'Wave height: {record["relative_wave_height"]:.2f} SWH',
             f'Record start time: {record["start_date"]}',
             f'Source file: {record["filename"]}',
+        ]
+        info_right = [
+            f'QC flags fired: {record["flags_fired"]}' if not qc_passed else 'QC passed',
         ]
 
         fig, ax = plt.subplots(1, 1, figsize=(15, 4))
@@ -55,9 +67,10 @@ def plot_qc(qcfile, outdir, plot_flags=tuple('abdg'), plot_extreme=True):
         ax.set_xticks(np.arange(mintime, maxtime, 10), minor=True)
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
-        ax.text(0.01, 1, '\n'.join(info), va='top', ha='left', transform=ax.transAxes)
+        ax.text(0.01, 1, '\n'.join(info_left), va='top', ha='left', transform=ax.transAxes)
+        ax.text(0.99, 1, '\n'.join(info_right), va='top', ha='right', transform=ax.transAxes)
         fig.tight_layout()
-        fig.savefig(os.path.join(outdir, f'fowd_qc_{i:0>4}.pdf'), bbox_inches='tight')
+        fig.savefig(os.path.join(outdir, f'{basename}_qc_{i:0>4}.pdf'))
         plt.close(fig)
 
         i += 1
